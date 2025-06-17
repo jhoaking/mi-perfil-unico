83143b0e55def87257b1d30578c7a1ee8f7d2cb0
jhoaking <peredoroj@gmail.com>
2025-06-17 14:44:21 -0400

    modelo para la conexion a la db y validaciones

---
+++ src/model/UserModel.js
@@ -0,0 +1,24 @@
+import { connect } from "../db.js";
+
+export class UserClass {
+  static guardarUser = async (github_id, username, avatar_url) => {
+    const query = `INSERT INTO "Users"(github_id,username,avatar_url,createdAt)VALUES($1,$2,$3,NOW())RETURNING * ;`;
+    const values = [github_id, username, avatar_url];
+    const result = await connect.query(query, values);
+    return result.rows[0];
+  };
+
+  static buscarPorId = async (github_id) => {
+    const query = `SELECT * FROM "Users" WHERE github_id = $1 LIMIT 1 `;
+    const result = await connect.query(query, [github_id]);
+    return result.rows[0];
+  };
+
+  static controlUser = async (github_id) => {
+    const query = `SELECT * FROM "USERS" WHERE github_id = $1`;
+    const result = await connect.query(query, [github_id]);
+    if (result.rowCount === 0) return null;
+
+    return result.rows[0];
+  };
+}
